0bd9ee5f47183e47c09bb982d88c0e3d6804ee00
Csaba Simandi <20855193+x1mandi@users.noreply.github.com>
2023-03-02 12:35:14 -0500

    Completed Train class - future parent class

--- lib/My/Trains/Train.pm
+++ lib/My/Trains/Train.pm
@@ -19,6 +19,43 @@ sub _initialize {
 	my ( $self, $arguments ) = @_;
 	my %arguments = %$arguments;  #Shallow copy
 	my $class = ref $self;
-
 	
+	my $name = delete $arguments{name};
+	unless ( defined $name ) { 
+		croak("$class requires a name to be set.");
+	}
+
+	my $id = delete $arguments{id};
+	unless ( defined $id ) { 
+		croak("$class requires a ID to be set.");
+	}
+	
+	my $size = delete $arguments{size};
+	unless ( defined $size ) { 
+		croak("$class requires a size to be set.");
+	}
+
+	$self->{attributes}{name} = $name;
+	$self->{attributes}{id}  = $id;
+	$self->{attributes}{size} = $size;
+
+	if ( my $remaining = join ' ,', keys %arguments _ {
+		croak("Unknown keys to $class\::new: $remaining");
+	}
 }
+
+sub get_name {
+	my $self = shift;
+	return $self->{attributes}{name};
+}
+
+sub get_id {
+	my $self = shift;
+	return $self->{attributes}{id};
+}
+
+sub get_size {
+	my $self = shift;
+	return $self->{attributes}{size};
+}
+
